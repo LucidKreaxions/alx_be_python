595afc4bec9cb072206105e01cf711778473c0ce
Steve Chege <chegekstephen@gmail.com>
2024-12-13 23:39:19 -0800

    Basic Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+subtraction = number1 - number2
+addition = number1 + number2
+multiplication = number1 * number2
+
+print(f"Addition of number1 and number2 is {addition}")
+print(f"Substration of number1 and number2 is {subtraction}")
+print(f"Multiplication of number1 and number2 is {multiplication}")
