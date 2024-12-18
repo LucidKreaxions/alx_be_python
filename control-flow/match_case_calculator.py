7521a2c95eec4d55bba817ce513fe8d2088a777c
Steve Chege <chegekstephen@gmail.com>
2024-12-18 02:39:16 -0800

    Match-case calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,27 @@
-# A simple calculator for practicing match-case
+# A simple calculator
 
-# prompt the user for the input
+# Prompt the user for the input
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-sign =  input("Choose the operation (+, -, *, /): ")
+sign = input("Choose the operation (+, -, *, /): ")
 
-def calculate(sign):
-    match sign:
-        case +:
-            return num1 + num2
-        case -:
-            return num1 - num2
-        case *:
-            return num1 * num2
-        case /:
+def calculate(num1, num2, sign):
+    if sign == '+':
+        return num1 + num2
+    elif sign == '-':
+        return num1 - num2
+    elif sign == '*':
+        return num1 * num2
+    elif sign == '/':
+        if num2 != 0:
             return num1 / num2
-        case -:
-            return "Invalid operand"
-print(sign)
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Invalid operand"
+
+# Call the function and print the result
+result = calculate(num1, num2, sign)
+print(result)
 
 
