ebe9b166e5f0a97c4e409ba7b739739cbaf5e2b9
Steve Chege <chegekstephen@gmail.com>
2024-12-22 03:01:17 -0800

    Daily reminder app

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,4 +1,5 @@
 # Daily_reminder app
+
 # Prompt for a Single Task
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
@@ -7,14 +8,13 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 # Process the Task Based on Priority and Time Sensitivity
 match priority:
     case 'high':
-        reminder = f"Reminder: '{task}' is a high priority task"
+        reminder = f"'{task}' is a high priority task"
     case 'medium':
-        reminder = f"Reminder: '{task}' is a medium priority task"
+        reminder = f"'{task}' is a medium priority task"
     case 'low':
-        reminder = f"Reminder: '{task}' is a low priority task"
+        reminder = f"'{task}' is a low priority task"
     case _:
-        reminder = "Invalid priority level entered."
-        print(reminder)
+        print("Invalid priority level entered.")
         exit()  # Exit if the priority is invalid
 
 # Modify the reminder based on time sensitivity
@@ -26,4 +26,4 @@ else:
     reminder += " Please specify if it's time-bound with 'yes' or 'no'."
 
 # Provide a Customized Reminder
-print(reminder)
+print(f"Reminder: {reminder}")
