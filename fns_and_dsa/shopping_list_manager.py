c005299bb913a7bb26ca62255dc2ef599bea948e
Steve Chege <chegekstephen@gmail.com>
2025-01-11 22:07:54 -0800

    Shopping list logic.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -19,13 +19,21 @@ def main():
             print(f"'{item}' has been added to the shopping list.")
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter an item to remove.")
-            shopping_list.remove(item)
-            print(f"'{item}' has been removed from the shopping list.")
+            item = input("Enter the item to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"'{item}' has been removed from the shopping list.")
+            else:
+                print(f"'{item} is not in the shopping list.")
         elif choice == '3':
             # Display the shopping list
-            for item in shopping_list[]:
-                print(item)
+            if shopping_list:
+                print("Shopping List:")
+                for index, item in enumerate(shopping_list, start=1):
+                    print(f"'{index}. {item}")
+            else:
+                print("Your shopping list is empty.")
+
         elif choice == '4':
             print("Goodbye!")
             break
