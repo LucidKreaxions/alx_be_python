70d083b0fe10ca436076eeb1de309f3fea886dbe
Steve Chege <chegekstephen@gmail.com>
2025-01-19 05:18:19 -0800

    try-exception task

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,16 @@
+def safe_divide(numerator, denominator):
+    """Perform  division and handle potential errors."""
+    try:
+        # Attempt to convert inputs to floats
+        num = float(numerator)
+        denom = float(denominator)
+
+
+        # Perform the division
+        result = num / denom
+        return f"The result of the division is {result}"
+
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Pleae enter numeric values only."
