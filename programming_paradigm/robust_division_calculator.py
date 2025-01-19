987de530fbce552f5b5fddf5c202a1b6d73df606
Steve Chege <chegekstephen@gmail.com>
2025-01-19 05:24:44 -0800

    try-exception task

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -13,4 +13,4 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Pleae enter numeric values only."
+        return "Error: Please enter numeric values only."
